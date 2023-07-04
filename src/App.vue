7f7a48eb5f972091e5db6f38be468401ff829442
mkarcz777 <karcz.marcin.artur@gmail.com>
2023-07-04 15:24:13 +0200

    Add basic hero image view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="app">
-    <nav class="nav">
-      <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
 
-<style lang="scss" scoped />
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700&display=swap');
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/components/SearchInputComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="searchWrapper">
+    <input id="search"
+       name="search"
+       placeholder="Search..."
+       v-model="searchValue"
+       @input="handleInput" />
+  </div>
+</template>
+<script>
+// eslint-disable-next-line
+export default {
+  name: 'SearchInputComponent',
+};
+</script>
+<style lang="scss" scoped>
+    .searchWrapper {
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+
+        label {
+            font-family: 'Montserrat', sans-serif;
+        }
+
+        input {
+            height: 30px;
+            border: 0;
+            border-bottom: 1px solid black;
+            background: none;
+        }
+
+    }
+</style>
