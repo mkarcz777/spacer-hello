fdba6a61f06aaf563d8c63d5287f469ecf1dbe5e
mkarcz777 <karcz.marcin.artur@gmail.com>
2023-07-07 14:38:56 +0200

    Add results

---
+++ src/components/HeroImageComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="heroImage" />
+</template>
+<script>
+export default {
+  name: 'HeroImage',
+};
+</script>
+<style lang="scss" scoped>
+  .heroImage {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    z-index: -1;
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size:cover;
+    background-position: 80% 0%;
+    &::after {
+      position: absolute;
+      content: '';
+      background: rgba(0,0,0,.2);
+      top: 0;
+      left: 0;
+      z-index: 1;
+      width: 100%;
+      height: 100vh;
+    }
+  }
+</style>
--- src/components/SearchInputComponent.vue
+++ src/components/SearchInputComponent.vue
@@ -1,35 +1,66 @@
 <template>
-  <div class="searchWrapper">
-    <input id="search"
-       name="search"
-       placeholder="Search..."
-       v-model="searchValue"
-       @input="handleInput" />
-  </div>
+  <input id="search"
+    name="search"
+    placeholder="Search..."
+    :class="{ dark }"
+    :value="value"
+    @input="handleChange" />
+    <!-- :class="{ dark }" - rownowazny zapisowi :class="{ dark: dark}" (ES6 pozwala skrocic).
+    Pierwszy dark to nazwa klasy, drugi to warunek (step === 1) -->
 </template>
 <script>
 // eslint-disable-next-line
 export default {
   name: 'SearchInputComponent',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
-    .searchWrapper {
+    input {
         margin-top: 50px;
         display: flex;
         flex-direction: column;
         width: 300px;
+        height: 30px;
+        border: 0;
+        border-bottom: 1px solid white;
+        background: none;
+        transition: box-shadow .3s ease-out;
+        color: white;
+        text-align: center;
+        font-size: 18px;
+        font-weight: 300;
 
-        label {
-            font-family: 'Montserrat', sans-serif;
+        @media (min-width: 1024px) {
+            font-weight: 400;
         }
 
-        input {
-            height: 30px;
-            border: 0;
-            border-bottom: 1px solid black;
-            background: none;
+        &:focus {
+            outline: none;
+            box-shadow: 0 10px 20px -8px rgba(255,255,255, .5);
         }
 
     }
+    .dark {
+          color: #1e3d4a;
+          border-bottom-color: #1e3d4a;
+
+          &:focus {
+            box-shadow: 0 10px 20px -8px rgba(#1e3d4a, .2);
+          }
+      }
 </style>
